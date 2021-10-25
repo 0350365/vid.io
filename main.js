c053fdd3ba96994d4b4993ee200c3e65345dd0c4
Jun Chao Yang <92341817+0350365@users.noreply.github.com>
2021-10-25 17:42:36 -0400

    testing live transcoding with ffmpeg

--- main.js
+++ main.js
@@ -3,11 +3,30 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const fs = require('fs');
+const ffprobe = require('ffprobe');
+const ffmpeg = require('fluent-ffmpeg');
 
 app.use(express.static(__dirname));
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
 
+app.get('/videos', (req, res) => {
+  const vidPath = `streamables/${req.query.id}`;
+  const range = req.headers.range;
+  console.log(range);
+  const vidSize = fs.statSync(vidPath).size;
+
+  ffmpeg(vidPath)
+    .format('matroska')
+    .videoCodec('copy')
+    .audioCodec('aac')
+    .on('error', (err, stdout, stderr) => {
+      console.log(err + ' ' + stdout + ' ' + stderr);
+    })
+    .pipe(res, { end: true });
+});
+
+/*
 app.get('/videos', (req, res) => {
   try {
     // Path and size of video
@@ -29,14 +48,14 @@ app.get('/videos', (req, res) => {
     };
 
     res.writeHead(206, headers);
-    const Stream = fs.createReadStream(vidPath, { start, end });
+    var Stream = fs.createReadStream(vidPath, { start, end });
 
     Stream.pipe(res);
   } catch (err) {
     console.log(`yo u messed up here: ${err}`);
   }
 });
-
+*/
 app.get('/names', (req, res) => {
   var fnames = [];
   fs.readdir('./streamables', (err, files) => {
